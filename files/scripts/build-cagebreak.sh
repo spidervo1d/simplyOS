5eff85626459b61ee46694e87a215b486963385a
spidervo1d <santinoalasianqn@gmail.com>
2026-07-22 12:10:08 -0300

    actualizar script por problemas con tmp/scripts

--- files/scripts/build-cagebreak.sh
+++ files/scripts/build-cagebreak.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Clonar y compilar Cagebreak
-cd /tmp
-git clone https://github.com/project-repo/cagebreak.git cagebreak
-cd cagebreak
-meson setup build --prefix=/usr -Dxwayland=true -Dman-pages=false
-ninja -C build
-ninja -C build install
+WORKDIR="$(mktemp -d)"
+trap 'rm -rf "$WORKDIR"' EXIT
 
+cd "$WORKDIR"
+
+git clone https://github.com/project-repo/cagebreak.git
+cd cagebreak
+
+git checkout 0d9d8ea34a6dc2f21c5c6176f3e0cf29887e2a88   # o el commit correspondiente
+
+meson setup build \
+    --prefix=/usr \
+    --buildtype=release \
+    -Dxwayland=true
+
+meson compile -C build
+meson install -C build
