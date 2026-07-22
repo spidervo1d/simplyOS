5e454792cf147558731e1256a08cf6833882f696
spidervo1d <santinoalasianqn@gmail.com>
2026-07-22 11:04:07 -0300

    change atomic base and bug fixes

--- files/scripts/build-cagebreak.sh
+++ files/scripts/build-cagebreak.sh
@@ -1,13 +1,26 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Crear directorio temporal y asegurar limpieza al terminar
 workdir="$(mktemp -d)"
 trap 'rm -rf "$workdir"' EXIT
 
 cd "$workdir"
-git clone https://github.com/project-repo/cagebreak.git
+
+# Clonar repositorio oficial de Cagebreak
+git clone --depth 1 https://github.com/project-repo/cagebreak.git
 cd cagebreak
 
-meson setup build -Dxwayland=true --buildtype=release
+# Compilar con Meson/Ninja (Habilitar XWayland)
+PKG_CONFIG_PATH=/usr/lib64/pkgconfig:/usr/share/pkgconfig \
+meson setup build \
+    -Dxwayland=true \
+    --buildtype=release
+
 ninja -C build
 meson install -C build
+
+# Volver a raíz y limpiar (aunque el trap ya lo hará)
+cd /
+rm -rf "$workdir"
+
