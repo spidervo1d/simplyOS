fd17229e373189f15fb7dfdd00b39ad89298a37a
spidervo1d <santinoalasianqn@gmail.com>
2026-07-22 20:01:29 -0300

    testing error without logs

--- files/scripts/build-cagebreak.sh
+++ files/scripts/build-cagebreak.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -xeuo pipefail
 
-WORKDIR="$(mktemp -d)"
-trap 'rm -rf "$WORKDIR"' EXIT
-
-cd "$WORKDIR"
+echo "===== INICIO ====="
 
 git clone https://github.com/project-repo/cagebreak.git
 cd cagebreak
 
-git checkout 0d9d8ea34a6dc2f21c5c6176f3e0cf29887e2a88   # o el commit correspondiente
+git checkout 0d9d8ea34a6dc2f21c5c6176f3e0cf29887e2a88
 
 meson setup build \
     --prefix=/usr \
     --buildtype=release \
     -Dxwayland=true
 
-meson compile -C build
-meson install -C build
+meson compile -C build --verbose
+
+DESTDIR=/ meson install -C build
+
+echo "===== FIN ====="
